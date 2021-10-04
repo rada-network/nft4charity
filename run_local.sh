3de30b666da44ad4ef42e2dd681f521e37a3b069
chinh <hoangdinhchinhls@gmail.com>
2021-10-05 06:42:32 +0700

    reinit

--- run_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove all containers starting w/ backend-
-docker rm -f `docker ps -aq -f name=backend-*`
-
-docker-compose down
-docker-compose --file docker-compose.local.yml build
-docker-compose --file docker-compose.local.yml up --force-recreate
