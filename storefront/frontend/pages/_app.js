ddd0d3dd39e973da037175af601579d0aae20055
Thang Le <thangle@steamforvietnam.org>
2021-10-13 10:36:21 -0500

    feat: added sample storefront

---
+++ storefront/frontend/pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import Link from 'next/link'
+
+function Marketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFT For Charity</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">
+              Home
+            </a>
+          </Link>
+          
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default Marketplace
