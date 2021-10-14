4f51d1e7e9e7b814bd9500970e054fe5d1a3e43d
khangt1k25 <khangruni@gmail.com>
2021-10-14 17:08:40 +0700

    feat: bring erc721 into store

--- smartcontract/contracts/Store.sol
+++ smartcontract/contracts/Store.sol
@@ -4,21 +4,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-import "./RadaNFTToken.sol";
+
 import "./RadaToken.sol";
 
 
-contract Store is Ownable, ReentrancyGuard{
+contract Store is Ownable, ReentrancyGuard, ERC721URIStorage {
     
 
-    mapping(uint256 => uint256) public itemPrices;
-    
-    RadaNFTToken public nftToken;
-    RadaToken public radaToken;
+    RadaToken radaToken;
 
     struct Campaign {
         address owner;
@@ -26,7 +24,7 @@ contract Store is Ownable, ReentrancyGuard{
         uint current;
         uint price;
         uint slot;
-        
+        bool ended;
     }
     struct Donor {
         address add;
@@ -38,46 +36,134 @@ contract Store is Ownable, ReentrancyGuard{
         uint amount;
     }
     
+
     uint private _campaign_count;
     uint private _nft_count;
 
-    mapping(address => bool) public ableToCreateCampaign;
+    mapping(address => bool) private whitelist;
     mapping(uint => Donor[]) public donorsOfCampaign;
     mapping(uint => Distributer[]) public distributersOfCampaign;
     mapping(uint => Campaign) public campaigns;
     mapping(uint => uint) public nftToCampaign;
-    
-    function setNFTContractAddress(address _rada_nft_address, address _rada_token_address) public onlyOwner {
-        nftToken = RadaNFTToken(_rada_nft_address);
-        radaToken = RadaToken(_rada_token_address);
-    }
 
-    function createCampaign(string memory _name, uint _slot, uint _price) public {
-        require(nftToken.isWhitelister(msg.sender) == true);
 
-        campaigns[_campaign_count] = Campaign(msg.sender, _name, 0, _slot, _price);
-
-        for(uint i=0; i<_slot; i++){
-            nftToCampaign[_nft_count] = _campaign_count;
-        }
-        _nft_count += _slot;
-        _campaign_count += 1;
-
-        nftToken.batchMint(_slot);
-    }
-
-    function donating(uint _campaign_id, uint _amount, uint _nft_id) public {
-        require(campaigns[_campaign_id].slot > 0, "Campaign still has slot");
-        require(_amount > campaigns[_campaign_id].price, "Donate amount must greater or equal than price");
-        require(nftToCampaign[_nft_id] == _campaign_id, "NFT token must of true campaign");
-
+    constructor () ERC721("Gift Rada", "RadaNFT") Ownable() {
         
-        radaToken.transferFrom(msg.sender, campaigns[_campaign_id].owner, _amount);
+    }
+    
+    modifier onlyWhitelister() {
+        require(
+            whitelist[msg.sender] == true,
+            "Ownable: caller is not in the whitelist"
+        );
+        _;
+    }
+    modifier onlyOwnerOfToken(uint _nftId){
+        require(
+            msg.sender == ownerOf(_nftId),
+            "You are not the owner"
+        );
+        _;
+    }
+    modifier onlyOwnerOfCampaign(uint _campaign_id) {
+        require(
+            msg.sender == campaigns[_campaign_id].owner,
+            "You are not owner of campaign"
+            );
+        _;
+    }
+    // set token interface 
+    function setRadaERC20Interface(address _radatoken) external onlyOwner {
+        radaToken = RadaToken(_radatoken);
+    }
+    
+    
+    // whitelist methods
+    function addWhitelister(address _user) external onlyOwner {
+        whitelist[_user] = true;
+    }
+    function removeWhitelister(address _user) external onlyOwner {
+        whitelist[_user] = false;
+    }
+    function isWhitelister(address _user) external view returns (bool) {
+        return whitelist[_user];
+    }
 
-        nftToken.transfer(campaigns[_campaign_id].owner, msg.sender, _nft_id);
+
+    // campaign methods
+    function createCampaign(string memory _name, uint _slot, uint _price) public onlyWhitelister {
+
+        campaigns[_campaign_count] = Campaign(msg.sender, _name, 0, _slot, _price, false);
+        
+        if(_slot > 0){
+            for(uint i=0; i<_slot; i++){
+                nftToCampaign[_nft_count] = _campaign_count;
+            }
+            batchMint(_slot);
+        }
+        
+        _campaign_count += 1;
+        
+    }
+
+    function donatingNFT(uint _campaign_id, uint _amount, uint _tokenId) public {
+        require(campaigns[_campaign_id].slot > 0, "Campaign has no nft slot");
+        require(_amount > campaigns[_campaign_id].price, "Donate amount must greater or equal than price");
+        require(nftToCampaign[_tokenId] == _campaign_id, "NFT token must of true campaign");
+
+        // Need to explain this point, transfer erc20 from "from" -> "to" by thirdparty?
+
+        // radaToken.transferFrom(msg.sender, campaigns[_campaign_id].owner, _amount);
+        campaigns[_campaign_id].current += _amount;
+
+        _transfer(campaigns[_campaign_id].owner, msg.sender, _tokenId);
+
 
         donorsOfCampaign[_campaign_id].push(Donor(msg.sender, _amount, false));
         
     }
-    
+    function donatingNormal(uint _campaign_id, uint _amount) public {
+        require(_amount > campaigns[_campaign_id].price, "Donate amount must greater or equal than price");
+                
+        // radaToken.transferFrom(msg.sender, campaigns[_campaign_id].owner, _amount);
+        campaigns[_campaign_id].current += _amount; 
+        donorsOfCampaign[_campaign_id].push(Donor(msg.sender, _amount, false));
+    }
+    function addDistributer(uint _campaign_id, address _distributer) public onlyOwnerOfCampaign(_campaign_id){
+        distributersOfCampaign[_campaign_id].push(Distributer(_distributer, 0));
+    }
+    function endingCampaign(uint _campaign_id) public onlyOwnerOfCampaign(_campaign_id){
+        campaigns[_campaign_id].ended = true;
+    }
+    // function distributing(uint _campaign_id, uint _distributer_id, uint _amount) public onlyOwnerOfCampaign(_campaign_id) {
+        
+    //     require(campaigns[_campaign_id].current >= _amount, "Campaign out of amount money");
+
+    //     require(campaigns[_campaign_id].ended, "Campaign is not ended");
+
+        
+    //     Campaign storage c = campaigns[_campaign_id];
+        
+    //     address _distributer_address = distributersOfCampaign[_campaign_id][_distributer_id].add;
+
+    //     radaToken.transferFrom(msg.sender, _distributer_address, _amount);
+
+    //     c.current -= _amount;
+
+    // }
+    // nft methods
+    function mint() internal onlyWhitelister {
+        _safeMint(msg.sender, _nft_count);
+        _nft_count += 1;
+    }
+    function batchMint(uint _slot) internal onlyWhitelister {
+        for (uint i = 0; i <= _slot; i++) {
+            mint();
+        }
+    }
+    function setTokenURI(uint _tokenId, string memory _tokenURI) external onlyOwnerOfToken(_tokenId) {
+        _setTokenURI(_tokenId, _tokenURI);
+    }
+
 }
+
